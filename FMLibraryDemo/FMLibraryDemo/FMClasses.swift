2468eab42cfd07589e3962a51c1ce70e76d85b8d
Dimidrol <dkonygin@gmail.com>
2015-05-01 18:44:37 +0300

    some fixes

---
+++ FMLibraryDemo/FMLibraryDemo/FMClasses.swift
@@ -0,0 +1,22 @@
+//
+//  FMClasses.swift
+//  FMLibraryDemo
+//
+//  Created by Dimidrol on 01.05.15.
+//  Copyright (c) 2015 Finch. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FMCell: UITableViewCell  {
+    
+    
+    convenience init(){
+        
+        self.init(style: UITableViewCellStyle.Default, reuseIdentifier: "FMCell")
+        
+    }
+    
+    
+}
---
+++ FMLibraryDemo/FMLibraryDemo/FMManager.swift
@@ -0,0 +1,22 @@
+//
+//  FMManager.swift
+//  FMLibraryDemo
+//
+//  Created by Dimidrol on 01.05.15.
+//  Copyright (c) 2015 Finch. All rights reserved.
+//
+
+import Foundation
+
+
+class FMManager {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
